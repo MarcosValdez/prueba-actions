710c2e88e8140752b810709c7d4d91e84f7eb48f
MarcosValdez <marcos.valdez333@hotmail.com>
2022-12-05 20:43:43 -0500

    prueba lint

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const app = new express()
 app.use(express.json())
 
 app.get('/prueba', (req, res) => {
-    res.status(200).json({
-        gawr: 'Hola mi HOME'
-    })
-})
-
-console.log("prueba test")
-console.log("prueba test 2")
+  res.status(200).json({
+    gawr: 'Hola mi HOME'
+  })  
+}) 
+ 
+console.log('prueba test')
+console.log('prueba test 2')
 
 
 const puerto = process.env.PORT | 8080
 /* start the server */
 app.listen(puerto, () => {
-    console.log(`La api esta en http://localhost:${puerto}`);
+  console.log(`La api esta en http://localhost:${puerto}`)
 })
 
-export default app;
+export default app
 
